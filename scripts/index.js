e404cd5f0ab08ba487be5f8d6a252ed2872e57d1
Anna <aniakoz99@gmail.com>
2020-03-05 21:52:06 +0100

    add section:  cooperation, about brand, map,

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,23 @@ document.addEventListener("DOMContentLoaded", function (event) {
     $(document).ready(function () {
         console.log("xx")
         new WOW().init();
+        const icon = L.icon({
+            iconUrl: "./img/markerWolf.png",
+            iconSize: [54, 54]
+        });
+
+        var map = L.map('map').setView([50.072358, 19.901357], 15);
+        L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png', {
+            maxZoom: 20,
+            attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        L.marker([50.072358, 19.901357], {
+            title: "Marker",
+            icon: icon
+        }).addTo(map);
+
+
     })
 })
 
