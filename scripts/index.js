2a298bf651826c8b9d807ad294437fbe4e81cdeb
Anna <aniakoz99@gmail.com>
2020-03-07 20:33:40 +0100

    add gallery-grid and FAQ section, change menu href

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,43 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-    $(document).ready(function () {
-        console.log("xx")
-        new WOW().init();
-        const icon = L.icon({
-            iconUrl: "./img/markerWolf.png",
-            iconSize: [54, 54]
-        });
+  $(document).ready(function () {
+    console.log("xx")
+    new WOW().init();
+    const icon = L.icon({
+      iconUrl: "./img/markerWolf.png",
+      iconSize: [54, 54]
+    });
 
-        var map = L.map('map').setView([50.072358, 19.901357], 15);
-        L.tileLayer(
-            "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png",
-            {
-              attribution:
-                '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-              subdomains: "abcd",
-              maxZoom: 19
-            }
-          ).addTo(map);
-          
-        L.marker([50.072358, 19.901357], {
-            title: "Marker",
-            icon: icon
-        }).addTo(map);
+    var map = L.map('map').setView([50.072358, 19.901357], 15);
+    L.tileLayer(
+      "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png",
+      {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+        subdomains: "abcd",
+        maxZoom: 19
+      }
+    ).addTo(map);
+
+    L.marker([50.072358, 19.901357], {
+      title: "Marker",
+      icon: icon
+    }).addTo(map);
 
 
-    })
+
+    var selectedClass = "";
+    $(".filter").click(function () {
+      selectedClass = $(this).attr("data-rel");
+      $("#gallery-wolf").fadeTo(100, 0.1);
+      $("#gallery-wolf div").not("." + selectedClass).fadeOut().removeClass('animation');
+      setTimeout(function () {
+        $("." + selectedClass).fadeIn().addClass('animation');
+        $("#gallery-wolf").fadeTo(300, 1);
+      }, 300);
+    });
+
+
+
+  })
 })
 
