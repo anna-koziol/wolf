27f465969556b0996230c4d21529c997743948d0
Anna Koziol <aniakoz99@gmail.com>
2020-10-26 21:00:42 +0100

    Change map zoom and fix display buttons to filter on mobile

--- scripts/index.js
+++ scripts/index.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
             iconSize: [54, 54]
         });
 
-        var map = L.map('map').setView([50.059720, 19.942035], 13);
+        var map = L.map('map').setView([50.059720, 19.942035], 12);
         L.tileLayer(
             "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png", {
                 attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
